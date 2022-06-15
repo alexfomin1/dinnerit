1274995ee1ca08576db8c880aba8e08332825f6f
alexfomin1 <103732309+alexfomin1@users.noreply.github.com>
2022-06-15 15:01:26 +0300

    guide update

--- dinnerit/guide/models.py
+++ dinnerit/guide/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Dish(models.Model):
+    
+    name_of_dish = models.CharField(
+        max_length=255,
+    )
+    cuisine = models.CharField(
+        max_length=255,
+    )
+    
+    
+    def __str__(self):
+        return ' -- '.join([
+            self.dish_name,
+            self.cuisine,
+        ])
